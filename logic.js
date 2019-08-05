e53f4586e9869ac988242febbd88808ae4874f26
Max Reinmueller <maxreinmueller@gmail.com>
2019-08-05 12:17:23 -0700

    pesudo coded javascript file L

---
+++ logic.js
@@ -0,0 +1,44 @@
+  // 1. configures firebase 
+  var firebaseConfig = {
+      apiKey: "AIzaSyBUz0ZwyJJUT60j1INAh8P47dR4mR-p_D8",
+      authDomain: "train-scheduler-9267d.firebaseapp.com",
+      databaseURL: "https://train-scheduler-9267d.firebaseio.com",
+      projectId: "train-scheduler-9267d",
+      storageBucket: "",
+      messagingSenderId: "475747952889",
+      appId: "1:475747952889:web:c5a4e116cf4b8582"
+  };
+
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    var database = firebase.database();
+
+// 2. button for adding employees
+
+    // Gets user data inputs
+
+    // creates local temporary object for storing user data
+
+    // Uploads temporary object to the database
+
+    // logs uploaded data to the console
+
+    // clears form text boxes
+
+// 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+
+    // stores everything in a variable
+
+    // console logs info
+
+    // makes time look nicer
+
+    // calculates minuets away
+
+    // creats new row
+
+    // append new row to the table
+
+
+
