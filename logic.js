42d60ca9110d84222417c41b0f41197674e6074c
Max Reinmueller <maxreinmueller@gmail.com>
2019-08-06 10:19:00 -0700

    user input geting logged in the database and displaying on the dom

--- logic.js
+++ logic.js
@@ -1,28 +1,33 @@
   // 1. configures firebase 
   var firebaseConfig = {
-      apiKey: "AIzaSyBUz0ZwyJJUT60j1INAh8P47dR4mR-p_D8",
-      authDomain: "train-scheduler-9267d.firebaseapp.com",
-      databaseURL: "https://train-scheduler-9267d.firebaseio.com",
-      projectId: "train-scheduler-9267d",
-      storageBucket: "",
-      messagingSenderId: "475747952889",
-      appId: "1:475747952889:web:c5a4e116cf4b8582"
+    apiKey: "AIzaSyBUz0ZwyJJUT60j1INAh8P47dR4mR-p_D8",
+    authDomain: "train-scheduler-9267d.firebaseapp.com",
+    databaseURL: "https://train-scheduler-9267d.firebaseio.com",
+    projectId: "train-scheduler-9267d",
+    storageBucket: "",
+    messagingSenderId: "475747952889",
+    appId: "1:475747952889:web:c5a4e116cf4b8582"
   };
 
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
     var database = firebase.database();
 
 // 2. button for adding employees
-$('#addTrain').on('click', function(event) {
+$("#addTrain").on('click', function(event) {
 event.preventDefault(); 
 
     // Gets user data inputs
     var trName = $('#trainName').val().trim();
     var trDestination = $('#trainDestination').val().trim();
     var trTime = $('#trainTime').val();
-    var trFrequency = $('trainFrequency').val();
+    var trFrequency = $('#trainFrequency').val();
+
+    // console.log(trName);
+    // console.log(trDestination);
+    // console.log(trTime);
+    // console.log(trFrequency);
 
     // creates local temporary object for storing user data
     var newTrain = {
@@ -31,12 +36,13 @@ event.preventDefault();
         time: trTime,
         frequency: trFrequency,
     }
+    // console.log(newTrain);
 
     // Uploads temporary object to the database
     database.ref().push(newTrain);
 
     // logs uploaded data to the console
-    console.log(newTrain);
+    // console.log(newTrain);
 
     alert("New Train added successfully!!");
 
@@ -44,21 +50,58 @@ event.preventDefault();
     $('#trainName').val("");
     $('#trainDestination').val("");
     $('#trainTime').val("");
-    $('trainFrequency').val("");
+    $('#trainFrequency').val("");
 });
 // 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val());
 
     // stores everything in a variable
+    var nameOfTrain = childSnapshot.val().name;
+    var destinationOfTrain = childSnapshot.val().destination;
+    var timeOfTrain = childSnapshot.val().time;
+    var frequencyOfTrain = childSnapshot.val().frequency;
 
     // console logs info
+    console.log(nameOfTrain);
+    console.log(destinationOfTrain);
+    console.log(timeOfTrain);
+    console.log(frequencyOfTrain);
 
     // makes time look nicer
 
+    // calculates next arrival
+    var arrival = (timeOfTrain.val() + frequencyOfTrain.val());
+    console.log(arrival);
+
     // calculates minuets away
 
-    // creats new row
-
+    // creates new row
+    var newRow = $("<tr>").append(
+        $("<td>").text(nameOfTrain),
+        $("<td>").text(destinationOfTrain),
+        $("<td>").text(frequencyOfTrain)
+    );
     // append new row to the table
+        $("#trainTable  > tbody").append(newRow)
+});
+
+
+  
+    // Prettify the employee start
+    // var empStartPretty = moment.unix(empStart).format("MM/DD/YYYY");
+  
+    // Calculate the months worked using hardcore math
+    // To calculate the months worked
+    // var empMonths = moment().diff(moment(empStart, "X"), "months");
+    // console.log(empMonths);
+  
+    // Calculate the total billed rate
+    // var empBilled = empMonths * empRate;
+    // console.log(empBilled);
+  
+
+  
 
 
 
